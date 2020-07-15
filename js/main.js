4ba8fd89ce2731a7909f5cbf0d94eaad97f83320
matthewbond-qz <matthew.bond@quizlet.com>
2020-07-14 20:18:52 -0700

    checkboxes done and some PWA background files

--- js/main.js
+++ js/main.js
@@ -90,5 +90,12 @@
     $('.drawer').drawer();
   });
 
+  // Service Worker
+
+  if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('sw.js')
+    .then(reg => console.log('service worker registered'))
+    .catch(err => console.log('service worker not registered', err));
+}
 
 }(jQuery);
